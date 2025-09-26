d920cc8d061cbe0e9b42b9c3891a73c7a9463df8
KhinChawShweYee <40794538@live.napier.ac.uk>
2025-09-26 23:03:01 +0630

    Developed get all current salaries from database function

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -206,6 +206,48 @@ public class App
     }
 
 
+
+    //Use case 1
+    /**
+     * Gets all the current employees and salaries.
+     * @return A list of all employees and salaries, or null if there is an error.
+     */
+    public ArrayList<Employee> getAllSalaries()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary "
+                            + "FROM employees, salaries "
+                            + "WHERE employees.emp_no = salaries.emp_no AND salaries.to_date = '9999-01-01' "
+                            + "ORDER BY employees.emp_no ASC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<Employee> employees = new ArrayList<Employee>();
+            while (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("employees.emp_no");
+                emp.first_name = rset.getString("employees.first_name");
+                emp.last_name = rset.getString("employees.last_name");
+                emp.salary = rset.getInt("salaries.salary");
+                employees.add(emp);
+            }
+            return employees;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get salary details");
+            return null;
+        }
+    }
+
+
     public static void main(String[] args)
     {
         // Create new Application
@@ -221,8 +263,16 @@ public class App
 
 
         // Example: Get all salaries for "Engineer"
-        ArrayList<Employee> engineers = a.getSalariesByRole("Engineer");
-        a.displayEmployees(engineers);
+//        ArrayList<Employee> engineers = a.getSalariesByRole("Engineer");
+//        a.displayEmployees(engineers);
+
+
+
+        // Extract employee salary information
+        ArrayList<Employee> employees = a.getAllSalaries();
+
+        // Test the size of the returned data - should be 240124
+        System.out.println(employees.size());
         // Disconnect from database
         a.disconnect();
     }
